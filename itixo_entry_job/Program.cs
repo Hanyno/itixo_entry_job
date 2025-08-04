92f58479991fb0064d250fb3d4fc4795f2edc175
janko <janko@ULTRAMEGARAKETA>
2025-08-04 20:34:01 +0200

    Přidejte soubory projektu.

---
+++ itixo_entry_job/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace itixo_entry_job
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
